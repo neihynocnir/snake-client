77255a782b6a1a8003ac81189b3500f2fe6c99b2
Neihy Rincon <neihy.rincon@gmail.com>
2020-04-08 18:08:10 +0000

    function client as a module

--- play.js
+++ play.js
@@ -1,26 +1,4 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '135.23.222.148', 
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.write(`hi snek`);
-  
-  conn.on('data', (data) => {
-  console.log("Incoming data:");
-  console.log(data);
-});
-
-  return conn;
-}
+const  connect = require('./client');
 
 console.log('Connecting ...');
 connect();
-/*
-Update the connect function to also attach an event handler to handle incoming data and console log it for the player. */
