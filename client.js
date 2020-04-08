fb40bc104fed0be0058eded2292e3186a5d708c1
Neihy Rincon <neihy.rincon@gmail.com>
2020-04-08 18:29:50 +0000

    adding name

--- client.js
+++ client.js
@@ -1,21 +1,31 @@
 const net = require('net');
-
+const name = 'Name: NR';
 
 const connect = function() {
   const conn = net.createConnection({ 
     host: '135.23.222.148', 
     port: 50541
   });
+
+  conn.write(`${name}`);
+
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
-  conn.write(`hi snek`);
+  conn.write(`Hi snek`);
   
   conn.on('data', (data) => {
-  console.log("Incoming data:");
-  console.log(data);
-});
+    console.log("server talking:");
+    console.log(data);
+  });
 
   return conn;
 }
 
 module.exports = connect;
+
+
+/* stdin.setEncoding('utf8');
+stdin.on('data', (input) => {
+    console.log("I wrote this:", input);
+    connection.write(`${name}: ${input}`);
+})*/
