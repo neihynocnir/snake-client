60ac7ad966b5fd5aa06fdd7058447c152261942b
Neihy Rincon <neihy.rincon@gmail.com>
2020-04-08 20:15:15 +0000

    moving

--- client.js
+++ client.js
@@ -1,22 +1,30 @@
 const net = require('net');
-const name = 'Name: NR';
 
 const connect = function() {
+  const name = 'Name: RR';
+  const up = 'Move: up';
+  const right = 'Move: right';
+  const down = 'Move: down';
+  const left = 'Move: left';
+
+  
   const conn = net.createConnection({ 
     host: '135.23.222.148', 
     port: 50541
   });
 
+  conn.setEncoding('utf8'); 
   conn.write(`${name}`);
 
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.write(`Hi snek`);
-  
+  setInterval(() => { conn.write(`${up}`)}, 5000);
+  setInterval(() => { conn.write(`${right}`)}, 1000);
+  setInterval(() => { conn.write(`${down}`)}, 1000);
+  setInterval(() => { conn.write(`${left}`)}, 1000);
+
   conn.on('data', (data) => {
-    console.log("server talking:");
-    console.log(data);
+    console.log('Received: ' + data);
   });
+
 
   return conn;
 }
@@ -24,8 +32,6 @@ const connect = function() {
 module.exports = connect;
 
 
-/* stdin.setEncoding('utf8');
-stdin.on('data', (input) => {
-    console.log("I wrote this:", input);
-    connection.write(`${name}: ${input}`);
-})*/
+/* 
+To accomplish this, inside the connect function, use the .on method on our conn object to register a "connect" handler (a callback function). In the callback, print a message for us (the player) to see - something like "Successfully connected to game server" will suffice
+*/
